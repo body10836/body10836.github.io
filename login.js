95f69b6af5f829202faa087f3435aeba6f32c562
yahiashamaa <yehiashamaa987@gmail.com>
2023-10-21 23:04:49 +0300

    My_additions_(yahia)

--- login.js
+++ login.js
@@ -1,9 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js";
-import { signOut } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js";
-
-
+import { getDatabase, set, ref } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js";
+import { getAuth, createUserWithEmailAndPassword,signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,10 +17,10 @@ const firebaseConfig = {
   measurementId: "G-2ZQ4V6LBMD"
 };
 
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app)
+const user = auth.currentUser;
 
 
 
@@ -33,21 +31,18 @@ var signin = document.getElementById("signin")
 signin.addEventListener('click', (e) => {
   var email = document.getElementById("emailInp").value;
   var password = document.getElementById("passInp").value;
-  signInWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      // Signed in 
-      const user = userCredential.user;
-      // ...
-      document.location = "/homepage.html";
-
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      alert("uh oh!")
-    });
+signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    alert("signed in" + user.displayName )
+    
+   
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    alert("uh oh!" + errorMessage + errorCode)
+  });
 })
-
-
-
-
+  
--- signup.js
+++ signup.js
@@ -1,7 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js";
-
+import { minidenticon } from 'https://cdn.jsdelivr.net/npm/minidenticons@4.2.0/minidenticons.min.js'
+import { getDatabase, set, ref, onValue } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js";
+import { getAuth, createUserWithEmailAndPassword , updateProfile, signInWithEmailAndPassword  } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,38 +21,71 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app)
-var user = document.getElementById("userReg")
+
+
 // Signup Function
 var reg = document.getElementById("register")
 
+
+const svgToDataURL = svgStr => {
+	const encoded = encodeURIComponent(svgStr)
+		.replace(/'/g, '%27')
+		.replace(/"/g, '%22')
+
+	const header = 'data:image/svg+xml,'
+	const dataUrl = header + encoded
+
+	return dataUrl
+}
+
+
 reg.addEventListener('click', (e) => {
+  var username = document.getElementById("userInp").value;
+  var pfp = minidenticon(username)
+  var photo = svgToDataURL(pfp)
   var email = document.getElementById("emailInp").value;
   var password = document.getElementById("passInp").value;
+  
+createUserWithEmailAndPassword(auth,email,password)
+  .then((userCredential) => {
+    // Signed up 
+    
+    const user = userCredential.user;
+    updateProfile(user, {
+      displayName: username,
+      photoURL: photo
+    }).then(() => {
+       alert("signed up " )
+       console.log(user)
 
-  createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-
-      var user1 = document.getElementById("userReg")
-
-      // Signup Function
-      var reg = document.getElementById("register")
-
-      // Signed up 
-      const user = userCredential.user;
+      
+    }).catch((error) => {
+      // An error occurred
       // ...
-      alert("signed up!")
-      document.location = "/index.html";
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // ..
-      alert("uh oh!" + error + errorMessage)
-      console.log(errorCode + errorMessage)
-
+      alert(error.message)
     });
+    const db = getDatabase(app);
+    set(ref(db, 'users/' + username), {
+        username: username,
+        email: email,
+        profile_picture : photo,
+        uid : user.uid,
+        created_at :  Date.now(),
+        last_login : null
+      });
+    
+    // ...
+  })
+  
+    
 
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+    alert("uh oh!"+ error + errorMessage)
+    console.log(errorCode+ errorMessage)
+
+  });
 });
 
-
-var log_out = document.getElementById('log_out')
